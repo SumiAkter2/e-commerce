d1f2c5ce97096ac287317a02879f9dfdad78242b
SumiAkter2 <akteralisha36@gmail.com>
2023-10-27 18:29:05 +0600

    added features info

--- src/Component/Home/Features.jsx
+++ src/Component/Home/Features.jsx
@@ -12,10 +12,32 @@ import { BiMessageRounded } from "react-icons/bi";
 import { MdFindInPage } from "react-icons/md";
 import { FaFileImport } from "react-icons/fa";
 import "./css/Feature.css";
+import { featureInfo } from "../info/Info";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 const Features = () => {
   return (
     <div className="mb-12">
       <div>
+        <div className="services-content my-5">
+          <div className="bg-black opacity-80 lg:h-96 text-white text-center mt-12">
+            <h1 className="lg:text-4xl text-2xl py-12 font-bold px-6 mt-6">
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+              ullam voluptatum voluptas natus possimus dicta temporibus fuga et
+              corporis accusamus?
+              <br />
+              <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Optio, doloribus. "
+            </h1>
+            <button className=" btn btn-primary btn-outline lg:w-56 my-6">
+              Learn More
+            </button>
+          </div>
+        </div>
         {/* ==========services banner============= */}
         <div className="mb-6 ">
           <div className="  text-primary text-center p-6">
@@ -27,7 +49,60 @@ const Features = () => {
               form to access our virtual assistant services.
             </p>
 
-            <div className="grid lg:grid-cols-5 grid-cols-2 gap-6 my-12 lg:mx-24 mx-12">
+            <Swiper
+              spaceBetween={20}
+              slidesPerView={4}
+              breakpoints={{
+                300: {
+                  slidesPerView: 3,
+                },
+                400: {
+                  slidesPerView: 3,
+                },
+                639: {
+                  slidesPerView: 3,
+                },
+                865: {
+                  slidesPerView: 4,
+                },
+              }}
+              navigation={true}
+              pagination={{
+                dynamicBullets: true,
+                clickable: true,
+              }}
+              autoplay={{
+                delay: 5500,
+                disableOnInteraction: false,
+              }}
+              modules={[Pagination, Autoplay, Navigation]}
+              className="lg:mx-12 mx-2"
+              style={{
+                "--swiper-pagination-color": "#1ba9ca",
+                "--swiper-pagination-bullet-size": "16px",
+                "--swiper-pagination-bullet-horizontal-gap": "6px",
+                "--swiper-pagination-bullet-marginBottom": "6px",
+                "--swiper-pagination-bullet-inactive-color": "black",
+              }}
+            >
+              {featureInfo.map((item) => (
+                <SwiperSlide key={item._id} item={item}>
+                  <div className="my-6">
+                    <div className="card ">
+                      <figure className="pt-6">
+                        <FaFileImport fill="#FF533D" size="35px" />
+                      </figure>
+                      <div className="card-body items-center justify-start text-center ">
+                        <h2 className="card-title text-primary text-3xl my-3">
+                          Data Entry
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            {/* <div className="grid lg:grid-cols-5 grid-cols-2 gap-6 my-12 lg:mx-24 mx-12">
               <div className="card ">
                 <figure className="pt-6">
                   <FaFileImport fill="#FF533D" size="35px" />
@@ -38,7 +113,7 @@ const Features = () => {
                   </h2>
                 </div>
               </div>
-              
+
               <div className="card ">
                 <figure className="pt-6">
                   <FaFileImport fill="#FF533D" size="35px" />
@@ -59,7 +134,7 @@ const Features = () => {
                   </h2>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         {/* ===========services content================== */}
@@ -76,20 +151,6 @@ const Features = () => {
             from worry about the what-ifs of life.
           </h1>
         </div> */}
-        <div className="services-content my-5">
-          <div className="bg-black opacity-80 lg:h-96 text-white text-center mt-12">
-            <h1 className="lg:text-4xl text-2xl py-12 font-bold px-6 mt-6">
-              "HIRING A VIRTUAL ASSISTANT AS A REMOTE STAFF DEFINITELY
-              ACCELERATES YOUR BUSINESS AND YOUR COMPANY FROM 0 TO 740
-              HORSEPOWER.
-              <br />
-              <br />A VIRTUAL ASSISTANT MAKES YOU RUN FASTER, FLY HIGHER!"
-            </h1>
-            <button className=" btn btn-primary btn-outline lg:w-56 my-6">
-              Learn More
-            </button>
-          </div>
-        </div>
       </div>
     </div>
   );
--- src/Component/info/Info.jsx
+++ src/Component/info/Info.jsx
@@ -1,4 +1,53 @@
-export const featureInfo = [{
+import { MdManageAccounts } from "react-icons/md";
+import { RiBankFill } from "react-icons/ri";
+import { BsArrowLeftRight } from "react-icons/bs";
+import { RiFundsLine } from "react-icons/ri";
+export const ManageAccount = <MdManageAccounts size="60px" color="#1ba9ca" />;
+export const Bank = <RiBankFill size="60px" color="#1ba9ca" />;
+export const trad = <BsArrowLeftRight size="60px" color="#1ba9ca" />;
+export const Fund = <RiFundsLine size="60px" color="#1ba9ca" />;
+
+export const featureInfo = [
+  {
     id: 1,
-    
-}]
+    icon: ManageAccount,
+    name: "Manage MAM Account",
+  },
+
+  {
+    id: 2,
+    icon: trad,
+    name: "Forex Trading Expertise",
+  },
+  {
+    id: 3,
+    icon: Fund,
+    name: "Profit-Sharing Model",
+  },
+  {
+    id: 4,
+    icon: Bank,
+    name: "Managed Forex Accounts",
+  },
+  {
+    id: 5,
+    icon: ManageAccount,
+    name: "Manage MAM Account",
+  },
+
+  {
+    id: 6,
+    icon: trad,
+    name: "Forex Trading Expertise",
+  },
+  {
+    id: 7,
+    icon: Fund,
+    name: "Profit-Sharing Model",
+  },
+  {
+    id: 8,
+    icon: Bank,
+    name: "Managed Forex Accounts",
+  },
+];
